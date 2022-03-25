4ef649b928cbec2489e6cfb5737a91f2c03d06cf
Kang Kujun <donato14@naver.com>
2022-03-25 16:25:55 +0900

    🎉 Add React-App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
