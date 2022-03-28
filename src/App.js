49e4e4ee2f009c0bd505e65bf3b0b4a843ce304c
Kang Kujun <donato14@naver.com>
2022-03-28 15:34:42 +0900

    🚀 Ending

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Data from './data';
 import Content from './components/Content';
 import { Link, Route, Switch } from 'react-router-dom';
 import Detail from './components/Detail';
+import axios from 'axios';
 
 
 function App() {
 
   let [shoes, setShoes] = useState(Data);
+  let [재고, 재고변경] = useState([10,11,12]);
 
   return (
     <>
@@ -20,8 +22,8 @@ function App() {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
-                  <Nav.Link> <Link to="/detail">Detail</Link></Nav.Link>
+                  <Nav.Link as={Link} to="/">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
                   <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -34,8 +36,6 @@ function App() {
             </Container>
           </Navbar>
         </div>
-        
-        
       <Switch>
       <Route exact path="/">
         <div className="jumbotron">
@@ -44,18 +44,33 @@ function App() {
             <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
           </div>
         </div>
-        <div className='container'>
-          <div className='row'>
+          <div className='container'>
+            <div className='row'>
               {
                 shoes.map((a, i) => {
                   return <Content shoes={shoes[i]} key={i}/>
                 })
               }
           </div>
+            <button className='btn btn-primary' onClick={() => {
+
+              //로딩중UI
+              // axios.post('serverURL', { id: 'codingapple', pw: 1234 });
+              
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+                .then((result) => {  
+                  //로딩중UI 삭제
+                  let data = result.data;
+                  setShoes([...shoes, ...data]);
+                })
+                .catch(() => {  
+                  console.log('실패했어요')
+                })
+            }}>더보기</button>
         </div>
       </Route>
       <Route path="/detail/:id">
-        <Detail shoes = {shoes} />
+        <Detail shoes = {shoes} 재고={재고} 재고변경={재고변경} />
       </Route>
 
       <Route path="/:id">
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import '../detail.scss';
@@ -13,6 +14,26 @@ let 제목 = styled.h4`
 `;
 
 function Detail(props) {
+  
+  let [alert, setAlert] = useState(true);
+  let [inputData, setInputData] = useState();
+  
+  useEffect(() => {
+    //컴포넌트가 마운트 되었을때, 
+    //update 될때 특정 코드 실행
+
+    //디테일 컴포넌트 로드시 바로 ajxa로 데이터를 가져오고 싶을때
+    //axios.get()
+
+    console.log(11111)
+    let timer = setTimeout(() => { setAlert(false) }, 2000);
+    // return ()=>{ 실행할코드~~~ } //Detail이라는 컴포넌트가 사라질때(unmount) 실행된다
+    return ()=>{ clearTimeout(timer) }
+  }, []);
+  
+
+  
+
 
   let { id } = useParams();
   let history = useHistory();
@@ -26,9 +47,19 @@ function Detail(props) {
         <제목 color={'blue'} className="red">상세페이지</제목>
       </박스>
 
-      <div className='my-alert'>
-        <p>재고가 얼마 남지 않았습니다</p>
-      </div>
+      { inputData }
+      <input onChange={(e)=>{ setInputData(e.target.value) }} />
+
+      {
+        alert === true
+          ?
+            (<div className='my-alert'>
+              <p>재고가 얼마 남지 않았습니다</p>
+            </div>)
+          :
+          null
+      }
+      
 
       <div className="row">
         <div className="col-md-6">
@@ -37,8 +68,11 @@ function Detail(props) {
         <div className="col-md-6 mt-4">
           <h4 className="pt-5">{ matchProduct.title }</h4>
           <p>{ matchProduct.content }</p>
-          <p>{ matchProduct.price } 원</p>
-          <button className="btn btn-danger">주문하기</button> 
+          <p>{matchProduct.price} 원</p>
+          
+          <Info 재고={props.재고} id={id}></Info>
+
+          <button className="btn btn-danger" onClick={() => { let stock = [...props.재고]; stock[0] = (stock[0] - 1); props.재고변경(stock) }}>주문하기</button> 
           <button className="btn btn-primary" onClick={() => { 
             history.goBack();
           }}>뒤로가기</button> 
@@ -47,5 +81,21 @@ function Detail(props) {
     </div> 
   )
 };
+
+function Info(props) {
+  return (
+    <p>재고 : {props.재고[props.id]}</p>
+  )
+}
+
+// //이전버전 (Lifecycle Hook)
+// class Detail2 extends React.Component {
+//   componentDidMount() {
+//     //생성될때, 렌더링 되자마자 Ajax 요청할때 사용하기도 함
+//   }
+//   componentWillUnmount() {
+//     //사라질때
+//   }
+// }
 
 export default Detail;
